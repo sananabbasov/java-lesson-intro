d94c7f9ed10147af5206eb1a4fd616c539340c99
sananabbasov <sanan_abbasov@list.ru>
2023-06-24 23:24:42 +0400

    Initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Output
+        System.out.println("Hello, Java");
+        System.out.println("First lesson");
+
+        // Variables
+        // DataTypes => String, byte, short, int, long, float, double, char, bool
+
+        String phone = "Telefon";
+        byte point = 127; // -128 to 127
+        short view = 2342; // -32768 to 32767
+        int age = 299999999; // -2 billion to 2 billion
+        long way = 12312; // -9 quintillion to 9 quintillion
+        float price = 4.6f;
+        double height = 1.87;
+        char symbol = 'S';
+        boolean IsReady = false;
+
+        System.out.println(phone);
+        System.out.println(point);
+        System.out.println(view);
+        System.out.println(IsReady);
+
+
+    }
+}
